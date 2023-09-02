80eb48da41d6d881b2331cdabdb72090536f1e92
Prince <pn3147399@gmail.com>
2023-09-02 11:02:23 +0530

    add projects

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let ul = document.getElementById("ul");
 let btn = document.querySelectorAll(".topic-button");
  let loader = document.getElementById("loader");
  let logo = document.getElementById("logo");
-
+let tricksBtn = document.querySelectorAll(".tricks-btn");
 
 let ctrl = true;
     btn.forEach((buttons)=>{
@@ -18,57 +18,96 @@ buttons.addEventListener("click",(e)=>{
           }
           loader.style.display = "grid";
        
+    // console.log(data)
 
+    document.getElementsByClassName('proBtn')[0].style.display = "flex";
+    tricksBtn[0].id = data;
+    tricksBtn[1].id = `${data}Tricks`;
+    tricksBtn[2].id = `${data}Projects`;
+     
+    tricksBtn.forEach((trickBtn)=>{
+        trickBtn.addEventListener("click",(a)=>{
+           let tData = a.target.id;
+        //   console.log(tData)
+         ul.innerHTML = null;     
 
-         
+          fetchFun(`${tData}`)
+        loader.style.display = "grid";
+
+        })
+    })
     
-setTimeout(()=>{
+   
 
-    fetch(`${data}/${data}.json`).then((res)=>{
+    // console.log(newData)
+    
+    fetchFun(`${data}`)
 
-        return res.json()
-    }).then((result)=>{
-        loader.style.display = "none";
-       
+document.getElementById('subject').innerHTML = data + " Topics"
+    // console.log(data + "ji")
+   
 
-        for(let i =0; i<result.length; i++){
-            let list = document.createElement("li");
-            list.className = "lists";
-            ul.appendChild(list);      
-        }
-        let li = document.querySelectorAll(".lists");   
-      
-            li.forEach((test,index)=>{
-               test.innerHTML = result[index].name; 
-               
-               
-               test.addEventListener("click",(e)=>{
-                console.log(e.target.innerHTML);
-
-                let synName = e.target.innerHTML;
-                document.getElementById("topic_name").innerHTML = synName;
-
-                fetch(`${data}/${synName}.txt`).then((res)=>{
-                    return res.text();
-                   }).then((result2)=>{
-                    document.getElementById("display_syn").innerHTML = result2;
-                    
-                   })
-
-               
-            collapse()
-                   
-                   ctrl = true
-
-
-               })
-            });
-    });
-    // console.clear()
-    document.getElementById("subject").innerHTML = e.target.innerHTML + " Topics"
-    },100)
     ul.innerHTML = null;     
-  }) });
+  }) 
+});
+
+
+
+
+function fetchFun(data,e){
+    setTimeout(()=>{
+
+        fetch(`${data}/${data}.json`).then((res)=>{
+    
+            return res.json()
+        }).then((result)=>{
+            loader.style.display = "none";
+           
+    
+            for(let i =0; i<result.length; i++){
+                let list = document.createElement("li");
+                list.className = "lists";
+                ul.appendChild(list);      
+            }
+            let li = document.querySelectorAll(".lists");   
+          
+                li.forEach((test,index)=>{
+                   test.innerHTML = result[index].name; 
+                   
+                   
+                   test.addEventListener("click",(e)=>{
+                    console.log(e.target.innerHTML);
+    
+                    let synName = e.target.innerHTML;
+                    document.getElementById("topic_name").innerHTML = synName;
+    
+                    fetch(`${data}/${synName}.txt`).then((res)=>{
+                        return res.text();
+                       }).then((result2)=>{
+                        document.getElementById("display_syn").innerHTML = result2;
+                        
+                       })
+
+                   
+                collapse()
+                       
+                       ctrl = true
+    
+    
+                   })
+                });
+
+
+               
+        });
+        // console.clear()
+
+        },100)
+}
+
+
+
+
 
   let bars = document.getElementsByClassName("fa-bars")[0];
   let sidebar = document.getElementsByClassName("alltopics")[0];
@@ -85,23 +124,3 @@ setTimeout(()=>{
     ctrl = false
 
     };
-
-    let date = new Date();
-
-
-    let userdata = date.getDay();
-
-    switch(userdata){
-        case 0: console.log("Sunday"); break;
-        case 1: console.log("Monday"); break;
-        case 2: console.log("Tuesday"); break;
-        case 3: console.log("Wednesday"); break;
-        case 4: console.log("Thursday"); break;
-        case 5: console.log("Friday"); break;
-        case 6: console.log("Saturday"); break;
-        default: console.log("nodata") ;
-    }
-
-
-
-
